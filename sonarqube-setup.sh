ce6e535d14c92bf9733faa7a2547f2b6e3aec364
mrnegmat <mr.negmat@gmail.com>
2023-02-14 13:08:33 +0500

    sonarqube file updated

--- sonarqube-setup.sh
+++ sonarqube-setup.sh
@@ -71,11 +71,12 @@ systemctl daemon-reload
 systemctl enable sonarqube.service
 #systemctl start sonarqube.service
 #systemctl status -l sonarqube.service
+
 apt-get install nginx -y
 rm -rf /etc/nginx/sites-enabled/default
 rm -rf /etc/nginx/sites-available/default
-cat <<EOT> /etc/nginx/sites-available/sonarqube
 
+cat <<EOT> /etc/nginx/sites-available/sonarqube
 server{
     listen      80;
     server_name sonarqube.groophy.in;
@@ -95,6 +96,7 @@ server{
     }
 }
 EOT
+
 ln -s /etc/nginx/sites-available/sonarqube /etc/nginx/sites-enabled/sonarqube
 systemctl enable nginx.service
 #systemctl restart nginx.service
